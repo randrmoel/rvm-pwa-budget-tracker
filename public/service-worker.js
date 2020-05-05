c0df31d9c24f5e472cb13c6aad8f7eb02236b72e
Robert Moel <robert.moel@rvmconsulting1.com>
2020-05-05 14:42:56 -0400

    tweaks

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,8 @@ const FILES_TO_CACHE = [
   "index.js",
   "style.css",
   "manifest.webmanifest",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png"
 
 ];
 
